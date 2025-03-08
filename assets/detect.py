dff081e48121dcf92ee155823912462839e01e50
cheesesnakes <shawn27.dsouza@pm.me>
2025-03-08 12:36:29 +0530

    fix detections

--- assets/detect.py
+++ assets/detect.py
@@ -5,120 +5,135 @@ import numpy as np
 
 # function to load model
 
+
 def load_model(video):
-    
     # load model
-        
+
     # load the yolo model (https://github.com/tamim662/YOLO-Fish/tree/main)
-    
-    video.net = cv2.dnn.readNetFromDarknet('model.cfg', 'model.weights')
-    
-    if video.useGPU: # run on GPU
-        
+
+    model_folder = "model/"
+    video.net = cv2.dnn.readNetFromDarknet(
+        model_folder + "model.cfg", model_folder + "model.weights"
+    )
+
+    if video.useGPU:  # run on GPU
         # Set the backend and target to CUDA
         video.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
         video.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
     # get the output layer names
-    
+
     video.layer_names = video.net.getLayerNames()
-    
-    video.output_layers = [video.layer_names[i - 1] for i in video.net.getUnconnectedOutLayers()]
-    
+
+    video.output_layers = [
+        video.layer_names[i - 1] for i in video.net.getUnconnectedOutLayers()
+    ]
+
     # get the class labels
-    
+
     video.model_classes = ["Fish"]
+
 
 # function to detect fish
 
+
 def detect_fish(video, frame):
-    
     # get the height and width of the frame
-        
+
     height, width, channels = frame.shape
-    
+
     # create a blob from the frame
-    
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    
+
+    blob = cv2.dnn.blobFromImage(
+        frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False
+    )
+
     # set the input
-    
+
     video.net.setInput(blob)
-    
+
     # get the output
-    
+
     outs = video.net.forward(video.output_layers)
-    
+
     # lists for non-max suppression
-    
+
     confidences = []
     class_ids = []
-    
+
     # loop through the detections
-    
+
     for out in outs:
         # Vectorize the detection processing
         scores = out[:, 5:]
         class_ids = np.argmax(scores, axis=1)
         confidences = scores[np.arange(len(scores)), class_ids]
-        
+
         # Filter out weak detections
         mask = confidences > 0.5
         confidences = confidences[mask]
         class_ids = class_ids[mask]
         detections = out[mask]
-        
+
         # create an array of boxes
-        
+
         boxes = np.zeros((len(detections), 4))
-        
+
         for i, detection in enumerate(detections):
-            
             center_x = int(detection[0] * width)
             center_y = int(detection[1] * height)
             w = int(detection[2] * width)
             h = int(detection[3] * height)
-            
+
             x = int(center_x - w / 2)
             y = int(center_y - h / 2)
-            
+
             boxes[i] = [x, y, w, h]
-    
+
     return boxes, confidences
+
 
 # draw boxes after non-max suppression
 
+
 def draw_fish(video, frame, boxes, confidences):
-    
     # get number of fish so far
-    
+
     fish_count = len(video.data)
-    
+
     # apply non-max suppression
-    
+
     indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.4)
-    
+
     fish_id = fish_count
-    
+
     boxes = [boxes[i] for i in indices]
-    
+
     for i, box in enumerate(boxes):
-        
         x, y, w, h = [int(i) for i in box]
-        
+
         # get the fish id
-        
+
         fish_id = fish_id + 1
-        
+
         # draw the box
-        
-        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), int(2/video.scale))
+
         label = f"Fish {fish_id}"
-        cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5/video.scale, (0, 255, 0), int(2/video.scale))
-        
+        cv2.putText(
+            frame,
+            label,
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2,
+        )
+
     return frame
 
+
 # function to determine intersection of boxes
+
 
 def iou_vectorized(box, t_boxes):
     x1, y1, w1, h1 = box
@@ -136,10 +151,11 @@ def iou_vectorized(box, t_boxes):
 
     return inter_area / union_area
 
+
 # function to track fish and draw a bounding box
 
+
 def track_fish(video, frame, boxes, confidences):
-    
     # apply non-max suppression
     indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
@@ -148,39 +164,41 @@ def track_fish(video, frame, boxes, confidences):
 
     # update trackers
     t_boxes = []
-    
-    # get number of fish 
-    
+
+    # get number of fish
+
     fish_id = len(video.data)
-    
+
     if len(video.trackers) > 0:
-        
         for tracker in video.trackers:
-            
             success, box = tracker.update(frame)
-            
+
             if success:
-                
                 t_boxes.append(box)
-                
+
                 # draw
-                
+
                 x, y, w, h = [int(i) for i in box]
-                
+
                 fish_id += 1
-                
-                #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), int(2/video.scale))
-                
+
                 label = f"Fish {fish_id}"
-                
-                cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5/video.scale, (0, 255, 0), int(2/video.scale))
-        
+
+                cv2.putText(
+                    frame,
+                    label,
+                    (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 255, 0),
+                    2,
+                )
+
     # check if any are detected
-    
+
     if len(boxes) == 0:
-            
-            return frame
-    
+        return frame
+
     # Convert t_boxes to a NumPy array for vectorized operations
     if len(t_boxes) > 0:
         t_boxes_np = np.array(t_boxes)
@@ -195,31 +213,36 @@ def track_fish(video, frame, boxes, confidences):
 
     # check for new fish
     for i, box in enumerate(new_boxes):
-
         x, y, w, h = [int(i) for i in box]
-        
+
         # create a tracker
-        
+
         tracker = cv2.TrackerKCF_create()
 
         # initialize the tracker
-        
+
         tracker.init(frame, (x, y, w, h))
 
         # add the tracker to the list
-        
+
         video.trackers.append(tracker)
 
         # fish id
-        
+
         fish_id += 1
-        
+
         # draw the box
-        
-        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), int(2/video.scale))
-        
+
         label = f"Fish {fish_id}"
-            
-        cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5/video.scale, (0, 255, 0), int(2/video.scale))
+
+        cv2.putText(
+            frame,
+            label,
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2,
+        )
 
     return frame
